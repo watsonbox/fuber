b98e8f839e5fc6ef5fba642cbf3122b869cad7a8
Howard Wilson <howard@watsonbox.net>
2014-07-14 17:51:08 +0200

    Stripe changes working with Parse Cloud Code

--- Fuber/PaymentViewController.swift
+++ Fuber/PaymentViewController.swift
@@ -6,16 +6,67 @@
 //  Copyright (c) 2014 Howard Wilson. All rights reserved.
 //
 
-class PaymentViewController : UIViewController, STPViewDelegate {
+let STRIPE_PUBLISHABLE_KEY = "pk_test_oF4LKxDonsSzaUf0CuyHUsP6"
+
+class PaymentViewController : UIViewController, STPViewDelegate, UIAlertViewDelegate {
     @IBOutlet var stripeCardView: STPView
+    @IBOutlet var payButton: UIButton
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         stripeCardView.delegate = self
+        stripeCardView.key = STRIPE_PUBLISHABLE_KEY
+    }
+    
+    // Dismiss the on screen keyboard when tapping outside of the stripe card view
+    @IBAction func viewTapped(sender: AnyObject) {
+        stripeCardView.paymentView.resignFirstResponder()
+    }
+    
+    // STPViewDelegate function called when card data changes with validity info
+    func stripeView(view: STPView!, withCard card: PKCard!, isValid valid: Bool) {
+        payButton.enabled = valid
     }
     
     @IBAction func cancel(sender: AnyObject) {
+        presentingViewController.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+    @IBAction func pay(sender: AnyObject) {
+        // TODO: Show progress HUD
+        
+        stripeCardView.createToken { (token: STPToken!, error: NSError!) in
+            if (error) {
+                self.hasError(error.localizedDescription)
+            } else {
+                self.hasToken(token)
+            }
+        }
+    }
+    
+    func hasError(message: String) {
+        UIAlertView(title: "Error", message: message, delegate: nil, cancelButtonTitle: "Okay").show()
+    }
+    
+    func hasToken(token: STPToken!) {
+        PFCloud.callFunctionInBackground("stripePayment", withParameters: ["token": token.tokenId, "amount": 100]) { (results: AnyObject!, error: NSError!) in
+            if (error) {
+                let message = error.userInfo["error"] as NSString
+                self.hasError("\(message) Please try again.")
+            } else {
+                self.successfulPayment()
+            }
+        }
+    }
+    
+    func successfulPayment() {
+        UIAlertView(title: "Success", message: "Your card was successfully charged", delegate: self, cancelButtonTitle: "Okay").show()
+    }
+    
+    // Handler for when success UIAlertView is dismissed
+    func alertView(alertView: UIAlertView!, clickedButtonAtIndex buttonIndex: Int) {
         presentingViewController.dismissViewControllerAnimated(true, completion: nil)
     }
 }
--- ParseCloudCode/cloud/main.js
+++ ParseCloudCode/cloud/main.js
@@ -1,6 +1,20 @@
+/* Initialize the StripeCloud Module */
+var Stripe = require('stripe');
+Stripe.initialize('sk_test_3h9cLoZqiN5qEvcYNZcYmZtj'); // FIXME: Don't commit live secret key to repo
 
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+Parse.Cloud.define("stripePayment", function(request, response) {
+  Stripe.Charges.create({
+    amount: request.params.amount,
+    currency: "EUR",
+    card: request.params.token
+  }).then(
+    function(param) {
+      console.log('Made successful card charge with Stripe!');
+      response.success("Stripe charge succeeded.");
+    },
+    function(error) {
+      console.log('Charging with stripe failed. Error: ' + error);
+      response.error(error.message);
+    }
+  )
 });
